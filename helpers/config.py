3c7004fda801cba24069547cea1b94f9223bad99
Saeed <saeedwaheed97@gmail.com>
2026-01-19 13:11:33 +0200

    added the configration for the LLM models

--- helpers/config.py
+++ helpers/config.py
@@ -9,6 +9,24 @@ class Settings(BaseSettings):
     FILE_DEFAULT_CHUNK_SIZE: int
     MONGO_DB_URI : str
     MONGO_DB_NAME: str
+
+    
+
+    OPENAI_API_KEY :str = None
+    OPENAI_API_URL :str = None
+    COHERE_API_KEY :str = None
+
+    GENERATION_BACKEND :str
+    EMBEDDING_BACKEND :str
+
+    GENERATION_MODEL_ID :str = None
+    EMBEDING_MODEL_ID :str = None
+    EMBEDING_MODEL_SIZE : int = None
+
+    INPUT_DEFALUT_MAX_CHARACTERS :int = None
+    GENERATION_DEFAULT_MAX_TOKENS :int = None
+    GENERATION_DEFAULT_TEMPRATURE :float = None
+    
     class Config:
         env_file = ".env"
 def get_settings():
